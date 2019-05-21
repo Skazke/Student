737c7928570e6f09f8c2d5f7d6e338f7294c9e1f
Skazke <50023972+Skazke@users.noreply.github.com>
2019-05-21 14:05:40 +0200

    GOGOGO

--- src/test/java/Testiki.java
+++ src/test/java/Testiki.java
@@ -1,56 +1,78 @@
 import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.junit.Test;
-import java.util.UUID;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 
 public class Testiki {
     WebDriver driver;
     String Skazke = "Skazke";
-    String Phone = "Phone";
+    String Phone = "";
 
-
-
-
-        @Test
-        public void someSimpleTest(){
-
-            UUID uuid = UUID.randomUUID();
-            String P = Phone + uuid;
-            String expectedTitle = "App Students";
-            driver = new ChromeDriver();
-            driver.get("http://37.59.228.229:3000/");
-            String title = driver.getTitle();
-            Assert.assertEquals(expectedTitle, title);
-            driver.findElement(By.cssSelector("a.add-user")).click();
-            driver.findElement(By.cssSelector("input#icon_prefix")).clear();
-            driver.findElement(By.cssSelector("input#icon_prefix")).sendKeys("Petya");
-            driver.findElement(By.cssSelector("input#icon_telephone")).clear();
-            driver.findElement(By.cssSelector("input#icon_telephone")).sendKeys(P);
-            driver.findElement(By.cssSelector("body > div.row > div.col.s9.main-content > a")).click();
-            Assert.assertTrue (driver.getPageSource().contains("Phone13404759-29ca-4332-ada8-927470b527c1"));
-            WebElement element = driver.findElement(By.id("user-list"));
-            System.out.println("Printing " + element.getText());
-
-            System.out.println(P + "     НОВЫЙ АЙДИ!!!");
-
-            Assert.assertEquals("78c7a846-6f33-4130-8238-46004eb017f9",element.getText());
-            
-            
-
-
-
-
-        }
-        @After
-
-    public void postTiasi(){
-        driver.quit();
-        }
+    @Before
+    public void Before() throws InterruptedException {
+        String expectedTitle = "App Students";
+        driver = new ChromeDriver();
+        driver.get("http://37.59.228.229:3000/");
+        Thread.sleep(5000);
+        String title = driver.getTitle();
+        Assert.assertEquals(expectedTitle, title);
 
     }
 
+    @Test
+    public void someSimpleTest() {
+        UUID uuid = UUID.randomUUID();
+        String P = Phone + uuid;
+        driver.findElement(By.cssSelector("a.add-user")).click();
+        driver.findElement(By.cssSelector("input#icon_prefix")).clear();
+        driver.findElement(By.cssSelector("input#icon_prefix")).sendKeys("Petygfhhfga");
+        driver.findElement(By.cssSelector("input#icon_telephone")).clear();
+        driver.findElement(By.cssSelector("input#icon_telephone")).sendKeys(P);
+        driver.findElement(By.cssSelector("body > div.row > div.col.s9.main-content > a")).click();
+        WebElement element = driver.findElement(By.id("user-list"));
+        if (element.getText().contains("fedb6da1-107c-4b43-bad1-98d5ac52839b")) {
+            System.out.println(" User YES! ");
+        }
+        else {
+            System.out.println("User With this ID is NO ");
+        }
+        System.out.println(P);
+
+
+    }
+    @Test
+    public  void    AAA (){
+        WebElement element = driver.findElement(By.id("user-list"));
+        if (element.getText().contains("fedb6da1-107c-4b43-bad1-98d5ac52839b")) {
+            System.out.println(" User YES! ");
+        }
+        else {
+            System.out.println("User NO ");
+        }
+
+    }
+    @Test
+    public void aaaa(){
+        List<WebElement> elements = driver.findElements(By.id("user-list"));
+        for (WebElement el: elements)  {
+            Assert.assertTrue(el.getText().contains("fedb6da1-107c-4b43-bad1-98d5ac52839b"));
+        }
+    }
+
+@After
+
+        public void postTiasi () {
+            driver.quit();
+        }
+
+    }
